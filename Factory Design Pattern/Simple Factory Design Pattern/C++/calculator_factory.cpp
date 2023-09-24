cf13fef1c016feaaa367e12146e85cfc8eae025d
Santanu Pradhan <54279288+santanupradhan2011@users.noreply.github.com>
2023-09-24 16:35:10 +0530

    Create calculator_factory.cpp

---
+++ Factory Design Pattern/Simple Factory Design Pattern/C++/calculator_factory.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+// Abstract Product
+class Calculator {
+public:
+    virtual double calculate(double a, double b) = 0;
+};
+
+// Concrete Products
+class Addition : public Calculator {
+public:
+    double calculate(double a, double b) override {
+        return a + b;
+    }
+};
+
+class Subtraction : public Calculator {
+public:
+    double calculate(double a, double b) override {
+        return a - b;
+    }
+};
+
+class Multiplication : public Calculator {
+public:
+    double calculate(double a, double b) override {
+        return a * b;
+    }
+};
+
+class Division : public Calculator {
+public:
+    double calculate(double a, double b) override {
+        if (b != 0) {
+            return a / b;
+        } else {
+            std::cerr << "Error: Division by zero." << std::endl;
+            return 0;
+        }
+    }
+};
+
+// Simple Factory
+class CalculatorFactory {
+public:
+    static Calculator* createCalculator(char operation) {
+        switch (operation) {
+            case '+':
+                return new Addition();
+            case '-':
+                return new Subtraction();
+            case '*':
+                return new Multiplication();
+            case '/':
+                return new Division();
+            default:
+                cerr << "Error: Invalid operation." << std::endl;
+                return nullptr;
+        }
+    }
+};
+
+int main() {
+    double a, b;
+    char operation;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> operation;
+
+    Calculator* calculator = CalculatorFactory::createCalculator(operation);
+
+    if (calculator) {
+        double result = calculator->calculate(a, b);
+        cout << "Result: " << a << " " << operation << " " << b << " = " << result << std::endl;
+        delete calculator;
+    }
+
+    return 0;
+}
